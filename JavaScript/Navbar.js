07f28ee639f416c689e806348e6f1a03ef46635d
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-02-26 00:02:36 +0530

    homepage

---
+++ JavaScript/Navbar.js
@@ -0,0 +1,56 @@
+function new1() {
+    document.getElementById("popup-1").classList.toggle("active");
+    
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+
+span.onmouseout = function () {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var moda = document.getElementById("mymode");
+var btn1 = document.getElementById("mybt");
+
+var span = document.getElementsByClassName("close1")[0];
+
+btn1.onmouseover = function () {
+    moda.style.display = "block";
+}
+
+span.onmouseout = function () {
+    moda.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == moda) {
+        moda.style.display = "none";
+    }
+}
+
+function submit2(){
+    if([]){
+        window.location.assign("../HTML/Cart.html");
+        console.log("shivam");
+    }
+    else{
+        window.location.assign("../HTML/Empty.html");
+        console.log("Upendr");
+    }
+}
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,7 @@
 
 
+
+
 let mockapis =  document.getElementById("mock")
 
 async function fetchdata(){
@@ -26,3 +28,126 @@ function displaycards(data){
         mockapis.append(card)
     })
 }
+
+
+let thumbnail2 = document.getElementsByClassName("thumbnail2")
+let slider2 = document.getElementById("slider2")
+let buttonRight2 = document.getElementById("slide-right2")
+let buttonLeft2 = document.getElementById("slide-left2")
+
+
+buttonLeft2.addEventListener("click",() => {
+
+    slider2.scrollLeft -= 150;
+
+  })
+
+  buttonRight2.addEventListener("click",() => {
+
+    slider2.scrollLeft += 150;
+ 
+  })
+  
+ 
+  const maxScrollLeft2 =  slider2.scrollWidth - slider2.clientWidth
+
+
+  function autoPlay1(){
+    if(slider2.scrollLeft > (maxScrollLeft2 - 1)){
+        slider2.scrollLeft -= maxScrollLeft2;
+
+    //  maxScrollLeft -= slider.scrollLeft
+    }else{
+        slider2.scrollLeft += 1;
+        
+    }
+}
+
+
+
+let thumbnail1 = document.getElementsByClassName("thumbnail1")
+let slider1 = document.getElementById("slider1")
+let buttonRight1 = document.getElementById("slide-right1")
+let buttonLeft1 = document.getElementById("slide-left1")
+
+
+buttonLeft1.addEventListener("click",() => {
+
+    slider1.scrollLeft -= 150;
+
+  })
+
+  buttonRight1.addEventListener("click",() => {
+
+    slider1.scrollLeft += 150;
+ 
+  })
+  
+ 
+  const maxScrollLeft1 =  slider1.scrollWidth - slider1.clientWidth
+
+
+  function autoPlay1(){
+    if(slider1.scrollLeft > (maxScrollLeft1 - 1)){
+        slider1.scrollLeft -= maxScrollLeft1;
+
+    //  maxScrollLeft -= slider.scrollLeft
+    }else{
+        slider1.scrollLeft += 1;
+        
+    }
+}
+
+
+
+  
+  //product
+
+  let thumbnail = document.getElementsByClassName("thumbnail")
+  let slider = document.getElementById("slider")
+  let buttonRight = document.getElementById("slide-right")
+  let buttonLeft = document.getElementById("slide-left")
+
+  buttonLeft.addEventListener("click",() => {
+
+    slider.scrollLeft -= 150;
+
+  })
+
+  buttonRight.addEventListener("click",() => {
+
+    slider.scrollLeft += 150;
+ 
+  })
+
+
+  const maxScrollLeft =  slider.scrollWidth - slider.clientWidth
+//   alert(maxScrollLeft)
+ 
+// AutoPlaySlider
+function autoPlay(){
+    if(slider.scrollLeft > (maxScrollLeft - 1)){
+        slider.scrollLeft -= maxScrollLeft;
+
+    //  maxScrollLeft -= slider.scrollLeft
+    }else{
+        slider.scrollLeft += 1;
+        
+    }
+}
+
+let play = setInterval(autoPlay,30)
+
+
+// Pause the slider
+
+for(let i=0; i<thumbnail.length; i++){
+    thumbnail[i].addEventListener("mouseover", ()=>{
+          clearInterval(play)
+    })
+    thumbnail[i].addEventListener("mouseout", ()=>{
+        return play = setInterval(autoPlay,30)
+  })
+}
+
+
